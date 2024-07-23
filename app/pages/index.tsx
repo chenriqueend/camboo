d4a35b6642d500a482a97c3f4ea5c47b8da51951
Carlos Henrique <carloshenriqueend@gmail.com>
2024-07-22 22:18:12 -0300

    Camboo - Project has started!♾️

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,20 +1,30 @@
-// pages/index.js
+// pages/index.tsx
+import { GetServerSideProps } from "next";
 import { supabase } from "../../utils/supabaseClient";
 
-export default function Home({ data }) {
+interface HomeProps {
+  data: any; // Substitua `any` pelo tipo correto dos seus dados, se souber
+}
+
+const Home: React.FC<HomeProps> = ({ data }) => {
   return (
     <div className="container mx-auto">
       <h1 className="text-2xl font-bold">Dados do Supabase:</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
-}
+};
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async () => {
   const { data, error } = await supabase.from("your_table").select("*");
 
   if (error) {
     console.log(error);
+    return {
+      props: {
+        data: [],
+      },
+    };
   }
 
   return {
@@ -22,4 +32,6 @@ export async function getServerSideProps() {
       data,
     },
   };
-}
+};
+
+export default Home;
